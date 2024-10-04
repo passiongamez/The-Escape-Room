8e84456c5e0793c27556277b5c8f1dc846cadd3b
pgzer0 <passiongamezero@gmail.com>
2024-10-04 16:07:43 -0500

    most key items done. working on spotlight and frame puzzle

---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+   public void DebugStatment(string message)
+    {
+        Debug.Log(message);
+    }
+}
